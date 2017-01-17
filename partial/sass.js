17df875a406c4aa3386cf4c3329c045be148740d
frontainer <frontainer@gmail.com>
2017-01-17 16:04:51 +0900

    css-loaderのオプションを外部から渡せるように

--- partial/sass.js
+++ partial/sass.js
@@ -2,11 +2,22 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const DEFAULT_OPTIONS = {
+  sourceMap: true,
+  importLoaders: true,
+  localIdentName: '[name]-[local]-[hash:base64:5]',
+  'import': true,
+  url: true
+};
 const autoprefixer = require('autoprefixer');
 
-module.exports = function() {
+module.exports = function (options) {
+  options = webpackMerge({}, DEFAULT_OPTIONS, options);
+
+  let query = Object.keys(options).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(options[k])}`).join('&');
   return {
     module: {
       rules: [
@@ -16,7 +27,7 @@ module.exports = function() {
           loader: ExtractTextPlugin.extract({
             fallbackLoader: 'style-loader',
             loader: [
-              'css-loader?sourceMap&importLoaders=1&localIdentName=[name]-[local]-[hash:base64:5]', // CSS Moduleの場合 &modules をつける
+              `css-loader?${query}`, // CSS Moduleの場合 &modules をつける
               'postcss-loader',
               'sass-loader'
             ]
