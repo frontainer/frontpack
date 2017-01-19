0cc15137bd2ad7b7a2d109e81b25f2ed32ab20ba
frontainer <frontainer@gmail.com>
2017-01-19 22:45:58 +0900

    optionから出力先を変更できるように

--- preset/@preset-dll/index.js
+++ preset/@preset-dll/index.js
@@ -1,22 +1,38 @@
 'use strict';
-
+const path = require('path');
 const webpack = require('webpack');
-module.exports = function() {
-  return {
+const webpackMerge = require('webpack-merge');
+const DEFAULT_OPTIONS = {
+  outputPath: 'public'
+};
+module.exports = function(options) {
+  options = webpackMerge({}, DEFAULT_OPTIONS, options);
+  const config = {
+    output: {
+      path: path.join(process.cwd(), options.outputPath),
+      publicPath: '/',
+      filename: "assets/js/[name].js",
+      jsonpFunction: 'fr',
+      library: '[name]_library'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.sass', '.scss', '.ejs', '.html'],
+      modules: [
+        path.join(process.cwd(), 'src'),
+        path.join(process.cwd(), 'node_modules')
+      ]
+    },
     plugins: [
       new webpack.DllPlugin({
         context: process.cwd(),
         path: '[name]-manifest.json',
         name: '[name]_library'
-      }),
-      new webpack.BannerPlugin({
-        banner: 'console.warn("This script is development version.");',
-        raw: true,
-        entryOnly: true
       })
     ],
+    stats: 'minimal',
     performance: {
       hints: false
     }
   };
+  return config;
 };
