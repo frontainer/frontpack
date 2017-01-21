4425e94604246ff6ca9f8713a31c05664494ac98
frontainer <frontainer@gmail.com>
2017-01-21 14:09:54 +0900

    preset-server追加

---
+++ preset/@preset-server/index.js
@@ -0,0 +1,41 @@
+'use strict';
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const historyApiFallback = require('connect-history-api-fallback');
+const DEFAULT_OPTIONS = {
+  server: {
+    host: 'localhost',
+    port: 3000,
+    server: {
+      baseDir: 'public'
+    },
+    watchOptions: {
+      ignoreInitial: true,
+      ignored: '*.map'
+    },
+    files: [
+      '!**/*.map',
+      'public/**/*'
+    ]
+  },
+  historyApiFallback: false,
+  file: {
+    log: false
+  }
+};
+module.exports = function(options = {}) {
+  options = webpackMerge({},DEFAULT_OPTIONS,options);
+  if (options.historyApiFallback) {
+    options.server.middleware = options.server.middleware || [];
+    options.server.middleware.push(historyApiFallback());
+  }
+  if (options.server.proxy) {
+    delete options.server.server;
+  }
+  return {
+    plugins: [
+      new BrowserSyncPlugin(options.server,{reload: false})
+    ]
+  };
+};
