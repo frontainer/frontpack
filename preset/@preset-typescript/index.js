d6627022986ea1717cfe67e45052c3b5abecb742
frontainer <frontainer@gmail.com>
2017-02-01 15:33:35 +0900

    lintはfrontpackで面倒見ない

--- preset/@preset-typescript/index.js
+++ preset/@preset-typescript/index.js
@@ -3,7 +3,7 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const DEFAULT_OPTIONS = {
-  lint: true
+
 };
 module.exports = function(options = {}) {
   options = webpackMerge({}, DEFAULT_OPTIONS, options);
@@ -15,25 +15,7 @@ module.exports = function(options = {}) {
           loader: 'awesome-typescript-loader?useWebpackText&useBabel&useCache&silent'
         }
       ]
-    },
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        options: {
-          tslint: {
-            emitErrors: true,
-            failOnHint: true
-          }
-        }
-      })
-    ]
+    }
   };
-  if (options.lint) {
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      exclude: /node_modules/,
-      loader: 'tslint-loader',
-      enforce: 'pre'
-    });
-  }
   return config;
 };
