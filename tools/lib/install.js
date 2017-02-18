1c1392dc92f125665f645214936fda15d0cebf9b
frontainer <frontainer@gmail.com>
2017-02-18 19:34:06 +0900

    npm installからyarn installに

--- tools/lib/install.js
+++ tools/lib/install.js
@@ -3,13 +3,10 @@ const getDirs = require('./getDirs');
 const execSync = require('child_process').execSync;
 
 console.log(chalk.blue('install - ./'));
-execSync(`npm i`, {
-  stdio: 'inherit'
-});
 getDirs('preset').then((list) => {
   list.forEach((d) => {
     console.log(chalk.blue(`install - ${d}`));
-    execSync(`npm i`, {
+    execSync(`yarn install`, {
       cwd: d,
       stdio: 'inherit'
     })
